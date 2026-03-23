e2c0282101ccf79825a90b4311bf7f7f3d23baf0
Abraham Contreras <abranz.contreras@gmail.com>
2026-03-23 14:27:23 -0300

    feat: implementa manejo centralizado de excepciones HTTP

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,8 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\ForceJsonAccept;
+use App\Exceptions\PacketNotFoundException;
+use App\Exceptions\InvalidStatusTransitionException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -17,6 +19,10 @@ return Application::configure(basePath: dirname(__DIR__))
             ForceJsonAccept::class,
         ]);
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
-        //
+    ->withExceptions(function ($exceptions) {
+        $exceptions->render(function (InvalidStatusTransitionException $e) {
+            return response()->json([
+                'message' => $e->getMessage()
+            ], 422);
+        });
     })->create();
