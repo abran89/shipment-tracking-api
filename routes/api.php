dd246a21a7ef1bfee5d692f0a56cc694ea709421
Abraham Contreras <abranz.contreras@gmail.com>
2026-03-24 10:18:51 -0300

    feat: agregar ruta webhook del transportista

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
-use App\Http\Controllers\PacketController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PacketController;
+use App\Http\Controllers\CarrierWebhookController;
+
 
 Route::prefix('packets')->group(function () {
     Route::get('/', [PacketController::class, 'index']);
@@ -9,3 +11,5 @@ Route::prefix('packets')->group(function () {
     Route::get('/{packet}', [PacketController::class, 'show']);
     Route::put('/{packet}/status', [PacketController::class, 'updateStatus']);
 });
+
+Route::post('/webhooks/carrier', CarrierWebhookController::class);
